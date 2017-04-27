c298412520083edf8ec50ab3e38ceda081d17616
underwanderer <makethedog@gmail.com>
2017-04-27 22:11:23 +0300

    Interfaces Record and Condition added

--- src/organozer/Storage.java
+++ src/organozer/Storage.java
@@ -5,13 +5,39 @@
  */
 package organozer;
 
+import java.util.Calendar;
+
 /**
  *
  * @author evgene
  */
 public interface Storage {
     
+    /**
+     * Has to store all types of record, so must take corresponding param
+     * 
+     * @param record
+     */
     public void store ( Record record );
     
     public List <Record> retrive ( Condition condition );
+}
+
+/**
+ * We have several types of records, so an hierarchy is possible
+ * But how to perform requirements of <method>store</method>?
+ * @author suomi
+ */
+interface Record {
+    boolean setCreationTime ( Calendar time );
+    //boolean setStartTime ( Calendar time );
+    //boolean setEndTime ( Calendar time );
+    boolean setTitle ( String title );
+    boolean setContent ( Content content );
+    
+    boolean addTag ( Tag tag );
+}
+
+interface Condition {
+    
 }
